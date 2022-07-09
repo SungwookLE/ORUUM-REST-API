92ea48128726329ba92bc03f0f6c274b511e6aaa
SungwookLE <joker1251@naver.com>
2022-07-09 17:47:19 +0900

    feat: nginx setting done

--- backend/settings/develop.py
+++ backend/settings/develop.py
@@ -10,7 +10,7 @@ with open(config_file) as f:
 SECRET_KEY = secrets["django_config"]["SECRET_KEY"]
 DEBUG = True
 # blank means 'localhost', '127.0.0.1'
-ALLOWED_HOSTS = ['oruum', 'localhost', '127.0.0.1', '3.38.30.153', '0.0.0.0']
+ALLOWED_HOSTS = ['oruum', 'localhost', 'web', '127.0.0.1', '3.38.30.153', '0.0.0.0']
 
 DATABASES = {
     'default': {
