fb8b54f863afda7ea4e34a09d9c02492b43b44e0
minju <aldrn316@gmail.com>
2022-12-07 21:24:21 +0900

    minor

--- backend/urls.py
+++ backend/urls.py
@@ -30,9 +30,9 @@ schema_url_patterns = [
 
 schema_view_v1 = get_schema_view(
     openapi.Info(
-        title="Open API",
+        title="ORUUM API",
         default_version='v1',
-        description="시스템 API",
+        description="ORUUM API Document",
         terms_of_service="https://www.google.com/policies/terms/",
     ),
     public=True,
